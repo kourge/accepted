bf19e619d5cbd3ce29b01db1a06e858ebf5663e1
Suman Jandhyala <sumanvyj@gmail.com>
2012-10-20 13:56:19 -0700

    Add ability to put indefinite numbers of extracurriculars, sports, and work experience

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,9 +1,36 @@
 $(document).ready(function() {
    $("#add_sat_subject").click(clone_sat_subject); 
+   $("#add_extracurricular").click(clone_extracurricular);
+   $("#add_sport").click(clone_sport);
+   $("#add_work").click(clone_work);
 });
 
 function clone_sat_subject() {
     var clone = $($(".sat_subject")[0]).clone();
     clone.children()[1].value = "";  
     $($(".sat_subject").last()).after(clone);
+}
+
+function clone_extracurricular() {
+    var clone = $($(".extracurricular")[0]).clone();
+    clone.children()[0].value = "";  
+    clone.children()[2].value = "";  
+    clone.children()[4].value = "";  
+    $($(".extracurricular").last()).after(clone);
+}
+
+function clone_sport() {
+    var clone = $($(".sport")[0]).clone();
+    clone.children()[0].value = "";  
+    clone.children()[2].value = "";  
+    clone.children()[4].value = "";  
+    $($(".sport").last()).after(clone);
+}
+
+function clone_work() {
+    var clone = $($(".work")[0]).clone();
+    clone.children()[0].value = "";  
+    clone.children()[2].value = "";  
+    clone.children()[4].value = "";  
+    $($(".work").last()).after(clone);
 }
