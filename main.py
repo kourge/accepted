7f17b4ca9333ccd608071b47f6ded43333fed53a
Suman Jandhyala <sumanvyj@gmail.com>
2012-10-19 22:12:11 -0700

    Add Jinja template support

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
-import web
-
+import web, os, jinja2
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 urls = (
     r'/.*', 'Hello',
 )
 
-
 class Hello(object):
     def GET(self):
-        return 'Herro, world!'
-
+        #return render_template('hello.html', name='world',)
+        template_values = {
+            'name' : 'Test Name',
+        }      
+        template = jinja_environment.get_template('templates/hello.html')
+        return template.render(template_values)
 
 app = web.application(urls, globals()).wsgifunc()
