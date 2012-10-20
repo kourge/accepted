c4c9433ec5f646cbd959a8b3f66d7a79f62a7a29
Wilson Lee <kourge@gmail.com>
2012-10-20 04:56:57 -0700

    Refactor controllers into separate files

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
 #!/usr/bin/env python
 import web
-import os
-import jinja2
-from google.appengine.api import users
 
-from models import *
 import auth
+import controller
+from profilecontroller import ProfileController
+from resumecontroller import ResumeController
 
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__))
-)
 
 urls = (
     r'/login', 'login',
     r'/logout', 'logout',
-    r'/profile', 'profile',
-    r'/resume', 'resume',
+    r'/profile', 'ProfileController',
+    r'/resume', 'ResumeController',
     r'/', 'index',
 )
 
-class renderer(object):
-    def GET(self):
-        return jinja_environment.get_template(
-            'templates/%s.html' % (self.__class__.__name__,)
-        ).render({
-            'user' : auth.user(),
-        })
 
-class profile(renderer):
-    def POST(self):
-      return repr(web.input().keys())
-
-class resume(renderer):
+class IndexController(controller.Controller):
     pass
 
-class index(renderer):
-    pass
 
 class login(object):
     def GET(self):
