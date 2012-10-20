7e080306827e36f2d465d5c1087c3333f9485860
Wilson Lee <kourge@gmail.com>
2012-10-19 22:06:26 -0700

    Add more models

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import web
+from models import *
 
 
 urls = (
--- models.py
+++ models.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python
 import web
 from google.appengine.ext import db
-from google.appengine.api import users
+
 
 class Profile(db.Model):
     """Models a profile."""
-    user = db.UserProperty()
-    age = db.IntegerProperty()
-    school = db.StringProperty()
+    # We should explicitly store the user ID, since the User object does not update
+    # accordingly when the user changes email address.
+    uid = db.IntegerProperty(required=True, indexed=True)
+
+    age = db.IntegerProperty(required=True)
+    school = db.StringProperty(required=True)
 
 
-class Score(db.Model):
-    pass
+class Activity(db.Model):
+    """An activity such as sports or extracurriculars."""
+    type = db.StringProperty(
+        required=True, choices=set(['sports', 'extracurriculars'])
+    )
+    name = db.StringProperty(required=True)
+    start_year = db.IntegerProperty(required=True)
+    end_year = db.IntegerProperty()
+
+
+def validate_sat_score(value):
+    if not (value <= 800 and value >= 200):
+        raise db.BadValueError()
+
+
+class SatScore(db.Model):
+    writing = db.IntegerProperty(required=True, validator=validate_sat_score)
+    reading = db.IntegerProperty(required=True, validator=validate_sat_score)
+    math = db.IntegerProperty(required=True, validator=validate_sat_score)
+
 
