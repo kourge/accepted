a4bdea004566a16494eb21edd8c31af1e0d251a6
Suman Jandhyala <sumanvyj@gmail.com>
2012-10-19 23:39:26 -0700

    Reorganize rendering routes

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python
 import web, os, jinja2
+from google.appengine.api import users
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 urls = (
-    r'/.*', 'Hello',
+    r'/', 'index',
 )
 
-class Hello(object):
+class index(object):
     def GET(self):
-        #return render_template('hello.html', name='world',)
+        user = users.get_current_user()
+        if not user:
+            name = "Guest"
+        else:
+            name = user.nickname()
         template_values = {
-            'name' : 'Test Name',
+            'name' : name,
         }      
-        template = jinja_environment.get_template('templates/hello.html')
+        template = jinja_environment.get_template('templates/index.html')
         return template.render(template_values)
 
 app = web.application(urls, globals()).wsgifunc()
