e0695b8e638c971759df84adbe4213556ad6c69a
Wilson Lee <kourge@gmail.com>
2012-10-20 00:26:32 -0700

    Introduce formal auth support

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import web, os, jinja2
+import auth
+
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 urls = (
+    r'/login', 'login',
+    r'/logout', 'logout',
     r'/.*', 'Hello',
 )
 
@@ -15,5 +19,16 @@ class Hello(object):
         }      
         template = jinja_environment.get_template('templates/hello.html')
         return template.render(template_values)
+
+
+class login(object):
+    def GET(self):
+        return auth.login(web.ctx.env.get('HTTP_REFERER'))
+
+
+class logout(object):
+    def GET(self):
+        return auth.logout(web.ctx.env.get('HTTP_REFERER'))
+
 
 app = web.application(urls, globals()).wsgifunc()
